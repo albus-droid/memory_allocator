fe74b0e7e61412fe5e86e3cce848b3ac6f0964c9
Albin <albinbabuvarghese@gmail.com>
2024-02-19 13:01:42 -0500

    Added a pthread lock and modified the code accordingly

--- mem_alloc.c
+++ mem_alloc.c
@@ -1,14 +1,5 @@
 typedef char ALIGN[16];
 
-void *malloc(size_t size)
-{
-	void *block;
-	block = sbrk(size);
-	if (block == (void*) -1)
-		return NULL;
-	return block;
-}
-
 union header {
 	struct header_t {
 		size_t size;
@@ -22,3 +13,48 @@ typedef union header header_t;
 
 header_t *head, *tail;
 
+pthread_mutex_t global_malloc_lock;
+
+void *malloc(size_t size)
+{
+	size_t total_size;
+	void *block;
+	header_t *header;
+	if (!size)
+		return NULL;
+	pthread_mutex_lock(&global_malloc_lock);
+	header = get_free_block(size);
+	if (header) {
+		header->s.is_free = 0;
+		pthread_mutex_unlock(&global_malloc_lock);
+		return (void*)(header + 1);
+	}
+	total_size = sizeof(header_t) + size;
+	block = sbrk(total_size);
+	if (block == (void*) -1){
+		pthread_mutex_unlock(&global_malloc_lock);
+		return NULL;
+	}
+	header = block;
+	header->s.size = size;
+	header->s.is_free = 0;
+	header->s.next = NULL;
+	if (!head)
+		head = header;
+	if (tail)
+		tail->s.next = header;
+	tail = header;
+	pthread_mutex_unlock(&global_malloc_lock);
+	return (void*)(header + 1);
+}
+
+header_t *get_free_block(size_t size)
+{
+	header_t *curr = head;
+	while(curr) {
+		if (curr->s.is_free && curr->s.size >= size)
+			return curr;
+		curr  = curr->s.next;
+	}
+	return NULL;
+}
